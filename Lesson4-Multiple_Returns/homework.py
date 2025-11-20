5f81e6aded67afcc273a1f249349120459365804
Jenna Kurian <jenkur28@bergen.org>
2025-11-20 08:56:52 -0500

    exceptions.py not finished

--- Lesson4-Multiple_Returns/homework.py
+++ Lesson4-Multiple_Returns/homework.py
@@ -1,9 +1,16 @@
 # question 1
-def search_user_database(user):
-    if user == "" or " ":
+# def search_user_database(user):
+#    if user == "" or " ":
+#        return None, "No search query", False
+    
+#    if not user.isalpha():
+#        return False, "Invalid Characters", False
+
+def search_user_database(query):
+    if not query or query.strip() == "":
         return None, "No search query", False
     
-    if not user.isalpha():
+    if not query.isalpha():
         return False, "Invalid Characters", False
     
 result, message, success = search_user_database("")
@@ -22,6 +29,7 @@ def analyze_book_pages(books):
         if pages > 500:
             has_long = True
     return total, total_pages, average_pages, has_long
+ 
 
 count, total, avg, has_long = analyze_book_pages([250, 180, 620, 310])
 print(count) # 4
---
+++ Lesson5-Exception_Handling/exceptions.py
@@ -0,0 +1,22 @@
+# Common Errors
+# 1 - Division by zero
+# print(10/0)
+
+# 2 - ValueError
+#print(int("abc"))
+
+# 3 - KeyError
+#grades = {"Rob": 90}
+#print(grades["Bill"])
+
+# 4 - IndexError
+#numbers = [1,2,3]
+#print(numbers[5])
+
+# 5 - NameError
+# print(total)
+# total = 10
+
+# 6 - SyntaxError
+#for i in range(5)
+ #   print(i)
